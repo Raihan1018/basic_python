b0c202f6e8cd32d6004495c6a14268c09146b49a
Raihan <raihanuddinahmed10@gmail.com>
2025-06-18 19:23:30 +0600

    additional and multiplication

--- operator_in-list.py
+++ operator_in-list.py
@@ -1,6 +1,17 @@
-fruits = ['Apple', 'Mango', 'peach' 'Orange', 'Watermelon', 'Grape']
+# fruits = ['Apple', 'Mango', 'peach' 'Orange', 'Watermelon', 'Grape']
 
-print('apple' in fruits)
-print('Apple' in fruits)
-print('pineapple' not in fruits)
-print('Apple' not in fruits)
+# print('apple' in fruits)
+# print('Apple' in fruits)
+# print('pineapple' not in fruits)
+# print('Apple' not in fruits)
+
+
+
+
+#  additional and multiplication in list
+
+a = [1,2,3]
+b = [4,5,6]
+
+print(a + b)
+print(a * 3)
