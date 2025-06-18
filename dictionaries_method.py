2b46e49a2612a7da725f7b103a8830caff2e6c97
Raihan <raihanuddinahmed10@gmail.com>
2025-06-18 20:47:06 +0600

    get() method in dict

---
+++ dictionaries_method.py
@@ -0,0 +1,19 @@
+people = {
+    "john": 32,
+    'rob': 50,
+    'tim': 20
+}
+
+# print(people.get('rob'))
+# print(people.get('raaaaob'))
+# print(people['rob'])
+# print(people['rjim'])
+
+
+
+
+# print(people['rabbbb'])
+# print(people.get('raaaaob'))
+
+# different between them: 
+# if we use get() -> if the key is not preset application will be crash or do not give any error. it will return None. But if we do not get() -> the program will be crash. So better or recommended is to use get()
