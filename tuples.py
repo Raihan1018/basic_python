76099fbbac1c22ec083c32ca992ade633966f607
Raihan <raihanuddinahmed10@gmail.com>
2025-06-18 20:06:03 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# list is mutable, tuples is  immutable
+fruits = ('apple', 'orange', 'mango', 'pineapple')
+
+# fruits[1] = 'peach' # => tuples is immutable, we will get an error
+# print(fruits)
+# print(fruits[1])
+
+print(fruits[1:4])
