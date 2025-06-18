6d9359a80a63c99899daa63ca442984d9159e30e
Raihan <raihanuddinahmed10@gmail.com>
2025-06-18 18:55:04 +0600

    in and not in operator in list

---
+++ operator_in-list.py
@@ -0,0 +1,6 @@
+fruits = ['Apple', 'Mango', 'peach' 'Orange', 'Watermelon', 'Grape']
+
+print('apple' in fruits)
+print('Apple' in fruits)
+print('pineapple' not in fruits)
+print('Apple' not in fruits)
