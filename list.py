c16a2f3919dd0798ad3003097d2675aa08b921af
Raihan <raihanuddinahmed10@gmail.com>
2025-06-18 18:41:06 +0600

    list and indexing

---
+++ list.py
@@ -0,0 +1,7 @@
+fruits = ['Apple', 'Mango', 'Orange', 'Watermelon']
+# +ve =>     0        1         2           3
+# -ve =>    -4       -3        -2          -1
+# +ve indexing start with 0, and -ve index start with -1
+
+print(fruits[3])
+print(fruits[-1])
